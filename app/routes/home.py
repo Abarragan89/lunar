1d510c238e4f50c4a33bf179003b3ec34442a4dc
Anthony Barragan <anthonybarragan@MacBook-Pro-2.local>
2022-12-17 15:58:48 -0800

    Feature: added sign up and login functionality. Users are created on database

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,30 +1,4 @@
-from flask import render_template, request,jsonify, session, Blueprint
-# import os
-# from functools import wraps
-# import jwt
-# from dotenv import load_dotenv
-# from app import app
-
-
-# load_dotenv
-
-
-# app.config['SECRET_KEY'] = os.getenv('JWT_SECRET')
-
-# Authentication Decorator
-# def token_required(func):
-#     @wraps(func)
-#     def decorated(*args, **kwargs):
-#         token = request.args.get('token')
-#         if not token:
-#             return jsonify({'Alert!': 'Token is missing!'}), 403
-#         try:
-#             payload = jwt.decode(token, key=app.config['SECRET_KEY'], algorithm="HS256")
-#         except:
-#             return jsonify({'Alert!': 'Invalid Token!'})
-#         return func(*args, **kwargs)
-#     return decorated
-
+from flask import Blueprint, render_template, session
 
 # Create Blueprint
 bp = Blueprint('site', __name__)
@@ -33,19 +7,13 @@ bp = Blueprint('site', __name__)
 # Home Route
 @bp.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
+    is_loggedin = session.get('loggedIn')
+    return render_template('index.html', loggedIn=is_loggedin)
+
+@bp.route('/login')
+def login():
+    is_loggedin = session.get('loggedIn')
+    if is_loggedin == True:
+        return render_template('/')
     else:
-        return render_template('index.html')
-    
-# practice route
-@bp.route('/test')
-def test():
-    return 'You are in the TEST route'
-
-
-# For Authenticated
-@bp.route('/auth')
-def auth():
-    return 'You are verified, welcome to dash'
-    
+        return render_template('login.html')
