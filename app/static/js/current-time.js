60a5b848078e469bc09b8ffa1e635f29325c742e
Anthony Barragan <anthonybarragan@MacBook-Pro-2.local>
2023-01-11 22:15:38 -0800

    Testing time zone with days.js

--- app/static/js/current-time.js
+++ app/static/js/current-time.js
@@ -1,7 +1,18 @@
-const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+// const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
-const current_timestamp = new Date().toLocaleString('en-US', { timeZone: timeZone})
+// const current_timestamp = new Date().toLocaleString('en-US', { timeZone: timeZone})
 
+const nth = function(d) {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+}
+const day_date = dayjs()
+const current_day_of_week = day_date.format('dddd')
+const current_date = day_date.format('D')
 
-
-document.getElementById('dashboard-current-time').textContent = current_timestamp
+document.getElementById('dashboard-current-time').textContent = `${current_day_of_week}, ${current_date}${nth(current_date)}`
